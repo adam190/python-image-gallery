30c9baada7a54d940583ae9174e9d55aad45ef8e
root <root@ip-172-31-31-205.us-west-1.compute.internal>
2020-06-07 20:28:22 +0000

    Modified db.py

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -17,6 +17,7 @@ def get_password():
 def connect():
     global connection
     connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
+    connection.set_session(autocommit=True)
 
 def execute(query, args=None):
     global connection 
@@ -24,18 +25,69 @@ def execute(query, args=None):
     if not args:
         cursor.execute(query)
     else:
-	cursor.execute(query,args)
+        cursor.execute(query, args)
     return cursor
 
-def main(): 
+def list_user():
+    global connection
+    res = execute('select * from users')
+    for row in res:
+        print(row)
+
+
+def add_user():
+    global connection
+    x = str(input("Username>"))
+    y = str(input("Password>"))
+    z = str(input("Full name>"))
+
+    res = execute("insert into users (username, password, full_name) values (%s, %s, %s)", (x, y, z))
+
+    return res
+
+
+def edit_user():
+    global connection
+    x = str(input("Username to edit>"))
+    y = str(input("New password (press enter to keep current)>"))
+    z = str(input("New full name (press enter to keep current)>"))
+
+    res = execute("update users set password=%s, full_name=%s where username=%s", (y, z, x))
+    return res
+
+
+def delete_user():
+    global connection
+    x = str(input("Enter username to delete>"))
+    y = str(input("Are you sure you want to delete " + x + "? "))
+    if y == 'Yes' or 'Y' or 'y' or 'yes':
+        res = execute("delete from users where username=%s", (x,))
+        print('Deleted.')
+        
+    return res
+
+
+def quitting():
+    print('Bye')
+
+
+def main():
     connect()
-    res = execute('select * from users')
-    for row in res:
-        print(row)
-    res = execute("update users set password=%s where username='fred'", ('banana',))
-    res = execute('select * from users')
-    for row in res:
-        print(row)
+    print("1) List Users\n2) Add user\n3) Edit user\n4) Delete user\n5) Quit")
+    x = int(input("Enter command>"))
+    if x == 1:
+        list_user()
+    elif x ==2:
+        add_user()
+        list_user()
+    elif x == 3:
+        edit_user()
+        list_user()
+    elif x == 4:
+        delete_user()
+        list_user()
+    elif x == 5:
+        quitting()
 	
 	
 if __name__ == '__main__':
