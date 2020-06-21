589a8ad298ba4aade43860f836cb64ad75e486c8
EC2 Default User <ec2-user@ip-10-0-1-116.us-west-1.compute.internal>
2020-06-21 22:51:33 +0000

    Updated prod scripts.

--- ec2-scripts/ec2-prod-1.0.sh
+++ ec2-scripts/ec2-prod-1.0.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 
 export IMAGE_GALLERY_SCRIPT_VERSION="1.0"
+CONFIG_BUCKET="edu.au.cc.imagegallery-config"
 
 #install packages
 sudo yum -y update
 sudo yum install -y python3 git
+
 
 #configure/install custom software
 cd /home/ec2-user
 sudo git clone https://github.com/adam190/python-image-gallery.git
 sudo chown -R ec2-user:ec2-user python-image-gallery
 sudo su ec2-user -l -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
+
 
 #start/enable services
 sudo systemctl stop postfix
