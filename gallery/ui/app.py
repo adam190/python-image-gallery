2b232f9d50124a7f0e3d6c030d5f57a8ec4c997c
adam <adam@Hippos-MacBook-Pro-6.local>
2020-07-04 12:49:27 -0700

    Added sessions to app.py

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -1,15 +1,46 @@
-from flask import Flask
-from flask import request
 from flask import render_template
-from gallery.ui import db
 from flask import redirect
-from ..data.useer import user
+from flask import session
+
+from ..data.user import User
 from ..data.postgres_user_dao import PostgresUserDAO
-from ..data.db import connect
+from ..data.db import connect 
 
 app = Flask(__name__)
 app.secret_key = b'lkj;alsdkjf'
 connect()
+
+def get_user_dao():
+	return PostgresUserDAO()
+
+@app.route('/storeStuff')
+def storeStuff():
+	session['session'] = 22
+	session['other things'] = 'bob'
+
+@app.route('/debugSession')
+def debugSession():
+	result = ""
+	for key,value in session.items():
+		result += key+"->"+str(value)+"<br />"
+	return result
+
+@app.route('/admin/executeDeleteUser/<username>')
+def executeDeleteUser(username):
+	get_user_dao().delete_user(username)
+	return redirect('/admin/users')
+
+@app.route('/admin/deleteUser/<username>')
+def deleteUser(username):
+	return render_template("confirm.html",
+				title="Confirm delete",
+				message="Are you sure you want to delete this user",
+				on_yes="/admin/executeDeleteUser/"+username,
+				on_no="/admin/users")
+
+@app.route('/admin/users')
+def users():
+	return render_template('users.html, users=get_user_dao().get_users())
 
 @app.route('/')
 def hello_world():
